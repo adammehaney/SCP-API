8daf0b87595ef8e6f42753f245ffb4e68d3dfcf8
Adam <adxmbusinessyt@gmail.com>
2021-09-22 21:22:45 -0600

    made code prettier (fixed python tabs)

--- server.py
+++ server.py
@@ -5,24 +5,24 @@ from bs4 import BeautifulSoup
 from flask import Flask, request, render_template
 
 class statuses:
-    class forbidden:
-        status = "403"
-        description = "You do not have access to this page."
-    class badRequest:
-        status = "400"
-        description = "You did not input a valid SCP name or number!"
+	class forbidden:
+		status = "403"
+		description = "You do not have access to this page."
+	class badRequest:
+		status = "400"
+		description = "You did not input a valid SCP name or number!"
 
 App = Flask('')
 
 @App.route('/')
 def home():
-    return "SCP API"
+	return "SCP API"
 
 def run():
-    App.run(host='0.0.0.0',port=8080)
+	App.run(host='0.0.0.0',port=8080)
 
 def error(status, description):
-    return render_template('errors/error.html', status=status, description=description), int(status)
+	return render_template('errors/error.html', status=status, description=description), int(status)
 
 if __name__ == "__main__":
 	print("System on")
@@ -41,27 +41,28 @@ def loadDocumentation():
 
 @App.route('/api/scps/<scp>')
 def getScp(scp=None):
-    if scp.isnumeric():
-        SCPLoad = requests.get(f"https://scp-wiki.wikidot.com/scp-{scp}")
+	if scp.isnumeric():
+		SCPLoad = requests.get(f"https://scp-wiki.wikidot.com/scp-{scp}")
 
-        if SCPLoad.ok:
-            Soup = BeautifulSoup(SCPLoad.text, 'html.parser')
-            Soup.prettify()
+		if SCPLoad.ok:
+			Soup = BeautifulSoup(SCPLoad.text, 'html.parser')
+			Soup.prettify()
 
-            Result = {
-                "found": False,
-                "scp_info": {
-                    "item_number": "",
-                    "object_class": "",
-                    "description": ""
-                }
-            }
+			Result = {
+				"found": False,
+				"scp_info": {
+					"item_number": "",
+					"object_class": "",
+					"description": ""
+				}
+			}
 
-            Div = Soup.find('div', {'id': 'page-content'})
-            Children = Div.findChildren("div" , recursive=False)
-            for Child in Children:
-                return Child
-        else:
-            return SCPLoad
-    else:
-        return error(statuses.badRequest.status, statuses.badRequest.description)
+			Div = Soup.find('div', {'id': 'page-content'})
+        	Children = Div.findChildren("div" , recursive=False)
+        	for Child in Children:
+				if Child.text == "Item #:":
+					Result["item_number"] == "999"
+		else:
+			return SCPLoad
+	else:
+		return error(statuses.badRequest.status, statuses.badRequest.description)
