610e53bd3c81e84df4a7a8d432d9f674cbccf077
Adam <59457164+Adxxmm@users.noreply.github.com>
2021-09-23 00:29:43 +0000

    added basic html, server setup

--- server.py
+++ server.py
@@ -1,22 +1,34 @@
 import requests
 import json
 import threading
-import beautifulsoup
-from flask import Flask, request
+import bs4 as beautifulsoup
+from flask import Flask, request, render_template
+
+class statuses:
+	class forbidden:
+		status = "403"
+		description = "You do not have access to this page."
 
 App = Flask('')
 
 @App.route('/')
 def home():
-    return "LOHS System Online"
+    return "SCP API"
 
 def run():
     App.run(host='0.0.0.0',port=8080)
 
-def keep_alive():
-    t = threading.Thread(target=run)
-    t.start()
+if __name__ == "__main__":
+	print("System on")
+	Thread = threading.Thread(target=run)
+	Thread.start()
 
-@App.route('/register', methods=['GET'])
-def register():
-    print(request.json)
+@App.route('/api/scps/', methods=['GET'])
+def getScp():
+	print("get")
+	print(request)
+	return render_template('errors/error.html', status=statuses.forbidden.status, description=statuses.forbidden.description), 403
+
+@App.route('/api', methods=['GET'])
+def loadDocumentation():
+	return render_template('documentation/index.html')
