00397e6986a1cdf8f90d310a7a52102f321e50c3
Adam <adxmbusinessyt@gmail.com>
2021-09-22 20:55:23 -0600

    fixed silly typos, started server implementation

--- server.py
+++ server.py
@@ -5,9 +5,12 @@ import bs4 as beautifulsoup
 from flask import Flask, request, render_template
 
 class statuses:
-	class forbidden:
-		status = "403"
-		description = "You do not have access to this page."
+    class forbidden:
+        status = "403"
+        description = "You do not have access to this page."
+    class badRequest:
+        status = "400"
+        description = "You did not input a valid SCP name or number!"
 
 App = Flask('')
 
@@ -18,17 +21,27 @@ def home():
 def run():
     App.run(host='0.0.0.0',port=8080)
 
+def error(status, description):
+    return render_template('errors/error.html', status=status, description=description), int(status)
+
 if __name__ == "__main__":
 	print("System on")
 	Thread = threading.Thread(target=run)
 	Thread.start()
 
 @App.route('/api/scps/', methods=['GET'])
-def getScp():
+def getScps():
 	print("get")
 	print(request)
-	return render_template('errors/error.html', status=statuses.forbidden.status, description=statuses.forbidden.description), 403
+	return error(statuses.forbidden.status, statuses.forbidden.description)
 
-@App.route('/api', methods=['GET'])
+@App.route('/api/', methods=['GET'])
 def loadDocumentation():
 	return render_template('documentation/index.html')
+
+@App.route('/api/scps/<scp>')
+def getScp(scp=None):
+    if scp:
+        return scp
+    else:
+        error(statuses.badRequest.status, statuses.badRequest.description)
